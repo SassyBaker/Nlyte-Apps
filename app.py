5e855e983baf7779a9df1e21d2518e57f466df79
Georgio Cha <92047233+SassyBaker@users.noreply.github.com>
2025-11-22 18:45:25 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, jsonify
 from apps.qr_code_generator import qr_code_generator_bp
 
@@ -18,4 +20,4 @@ def health_check():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
